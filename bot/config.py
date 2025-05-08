3f4b4d65eced6a0c1eb2421b0632d36b73ac309c
ToukaSama <blacksummoner6659@gmail.com>
2025-05-08 20:41:51 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,9 +4,9 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = config("20415731", cast=int)
+    API_HASH = config("bd08752ff079fbe5f4f8497868b9a40e")
+    BOT_TOKEN = config("7602691098:AAGQ1VzEF69qlhnMwg3uWjYUB6oc1aGmh1Y")
     DEV = 1664850827
     OWNER = config("OWNER")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
